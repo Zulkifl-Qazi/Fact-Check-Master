5ea9c2bd1ee807c894e0e0feae20db6b7a4673ca
Zulkifl-Qazi <batch22.cys.00013@gmail.com>
2026-01-28 13:00:30 +0500

    Fix build issue: upgrade react-quill and update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-quill']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/react-quill/, /node_modules/]
+    }
+  },
   server: {
     proxy: {
       '/api': {
